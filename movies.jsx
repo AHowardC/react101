82437b40cb125cdb23f7df795cecc50635eb0328
AHowardC <achsvgusaeng@yahoo.com>
2017-11-22 13:37:44 -0500

    add movies files

---
+++ movies.jsx
@@ -0,0 +1,45 @@
+
+class Movies extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			moviesToShow: []
+		}
+	}
+
+
+	componentDidMount(){
+		console.log('Movies was just put into the DOM');
+	}
+
+	render(){
+		return(
+			<div className="container">
+                <div className="row">
+                    <div className="col-sm-12 text-center">
+                        <div className="th-wrapper">
+                            <button className="btn btn-primary">Reset Search</button>
+                        </div>
+                        <div className="movie-rows">
+                            {/* Movies go here! */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+			)
+	}
+}
+
+
+
+
+
+
+
+
+1. What
+2. Where
+ReactDOM.render(
+	<Movies/>,
+	document.getElementById('root')
+	)
